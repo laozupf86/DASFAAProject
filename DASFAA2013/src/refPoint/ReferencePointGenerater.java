f762243183bfaade02d1fc5faec315342ec584cb
Bob W <oldtong2@hotmail.com>
2012-10-15 17:00:02 +1000

    add

--- DASFAA2013/src/refPoint/ReferencePointGenerater.java
+++ DASFAA2013/src/refPoint/ReferencePointGenerater.java
@@ -3,7 +3,10 @@ package refPoint;
 public class ReferencePointGenerater {
 	
 	
-	double x;
+	private static double maxOfX = 117.5;
+	private static double maxOfY = 40.98;
+	private static double minOfX = 115.42;
+	private static double minOfY = 39.46;
 	
 	
 	public ReferencePointGenerater(){
